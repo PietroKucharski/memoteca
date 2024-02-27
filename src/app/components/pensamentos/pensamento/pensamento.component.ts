19cd809f14253ce21b1e920813fa8c4313afd46f
PietroKucharski <pietrokucharski@hotmail.com>
2024-02-27 13:56:16 -0300

    initial commit

---
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css'],
+})
+export class PensamentoComponent implements OnInit {
+  @Input() pensamento: Pensamento = {
+    id: 1,
+    conteudo: 'I love angular',
+    autoria: 'Pietro',
+    modelo: 'modelo2',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    }
+
+    return 'pensamento-p';
+  }
+}
